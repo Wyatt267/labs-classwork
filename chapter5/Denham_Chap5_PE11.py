fe2588d5ef6158b43786b62b81b487eeb6e57306
Wyatt267 <96692837+Wyatt267@users.noreply.github.com>
2022-02-24 10:25:44 -0600

    Functions added

--- chapter5/Denham_Chap5_PE11.py
+++ chapter5/Denham_Chap5_PE11.py
@@ -4,38 +4,59 @@
 #Importing Python module (random)
 import random
 
+#
 #Display info       
 print('This is a program that quizzes you using addition problems.')
 
-# Generate random addends using random.randint(0,999)
-firstAddend =  random.randint(0,999)
-secondAddend =  random.randint(0,999)
 
-# Calculate answers to the actual randomly generated problems
-correctNum = firstAddend + secondAddend
+def main ():
+    createRand ()
+    num1, num2 = getRandNum
+    dispProblem (num1, num2)
+    userInput = getInput ()
+    inspectAnswer (userInput, num1, num2)
+    endMessage ()
+    print()
+    input('End of Program. Press Enter key to end.')
+
+# Function generate random addends using random.randint(0,999)  
+def createRand()
+    firstAddend =  random.randint(0,999)
+    secondAddend =  random.randint(0,999)
+    return firstAddend, secondAddend
+
+
+
+
 
 #Format both random integers previously generated into addition problem
 #turn both values into strings
+def dispProblem ()
+    print(' ' , firstAddend)
+    print('+' , secondAddend)
+    print('------')
+    #Formatting edit needed here***
 
-print(' ' , firstAddend)
-print('+' , secondAddend)
-print('------')
+# Calculate answers to the actual randomly generated problems
 
 
 # Prompt user for answer
-userInput = int(input('What is the correct answer? '))
-print()
-
-#Validate user input (no strings or floats)
-#while userInput != int :
-#    userInput = int(input('Invalid answer. Please enter a whole number'))    #This area needs fixing
-
+def getInput ()
+    userInput = int(input('What is the correct answer? '))
+    print()
+    return userInput
 
 #Evaluate user input vs correctNum
-if userInput != correctNum:             #If incorrect print incorrect response and correct answer
-    print('That answer is incorrect. The correct answer is ', correctNum , '.')
-else:
-    print('Correct! Good Job! ')        #If correct print correct response
+def inspectAnswer ()
+    correctNum = firstAddend + secondAddend
+    if userInput != correctNum:                            #If incorrect print incorrect response and correct answer
+        print('That answer is incorrect. The correct answer is ', correctNum , '.')
+    else:
+        print('That amswer is correct! Good Job! ')        #If correct print correct response
+    
+    
+    
+def endMessage ()
 print()
 print('End of Program.')
 
