043ff5a190303f768d7221a53a00934ecbfd181c
Wyatt267 <96692837+Wyatt267@users.noreply.github.com>
2022-03-12 22:18:35 -0600

    WIP 2 lab 6

--- chapter6/Denham_Chap6_PE11.py
+++ chapter6/Denham_Chap6_PE11.py
@@ -7,33 +7,36 @@
 ###################################################################################
 
 # Display project info
+
+    
+    
+from ctypes import HRESULT
+
+
 print("This program creates a personal webpage with user-entered information. ")
 print()
 
 # Main Function
 def main():
-    html_file = open('Denham_page.html' , 'w')
-    while name and description != '  ':
-        try:
-            name = input("Enter your name: ")
-        except ValueError:
-            print("Enter a valid name (letters only): ")
-        except Exception as err:
-            print()
-            print(name, "is not a valid name. Enter a valid name (letters only): ")
-        else:
-            try:
-                description = input("Describe yourself: ")
-            except ValueError:
-                print("Enter a valid description (letters only): ")
-    html_file.write(str(name)+ '/n')
-    html_file.write(str(description)+ '/n')
-    end_message()
-                
+    display_program_documentation()
+    name, description = get_user_input()
+    html_file  = create_html_file('Denham_page.html')
+    html_content  = create_html_content(get_user_input.name, get_user_input.description)
+    write_html_content_to_file(html_content)
+    html_file.close()
+    display_html(html_content)
+    
     #create_website(name, description)
     
     
-    
+def display_program_documentation():
+    print("####################################################################################")
+    print("Developer Name:            Wyatt Denham ")
+    print("Program Name:              Denham_Chap6_PE11.py ")
+    print("Due Date:                  03/11/2022 ")
+    print("Program Description:       This program uses input from the user to generate a ")
+    print("                           personal web page. ")
+    print("###################################################################################")    
     
     
     
@@ -50,22 +53,71 @@ def get_user_input():
         try:
             description = input("Describe yourself: ")
         except ValueError:
-            print("Enter a valid description (letters only): ")
-            
-            
+            print("Enter a valid description (letters only): ") 
+    return name, description
+                  
+
+def create_html_file():
+    html_file = open('Denham_page.html' , 'w')
+    return 'Denham_page.html'
+    
+    
+    
+    
+    
+def create_html_content(get_user_input.name, get_user_input.description):
+     html_content = 
+    '<html>
+    <head>
+    <title>My Personal Web Page</title>
+    </head>
+    <body>
+        <center>
+# ...more html tags...
+            <div>${get_user_input.name}</div>
+        </center>
+        <hr />
+# ...more html tags....
+        <div>${get_user_input.description}</div>
+        <hr />
+# ...more html tags...
+    </body>
+    </html>'
+    
+    
+    
+    
+    
+    
+def write_html_content_to_file():
+    html_file.write()
+    
+    html_file.close()
+    
+def display_html(): 
+    print("<html>")
+    print("<head>")
+    print("<title>My Personal Web Page</title>")
+    print("</head>")
+    print("<body>")
+    print("    <center>")
+# ...more html tags...
+    print(         "<div>${get_user_input.name}</div>")        
+    print("    </center>")    
+    print("    <hr />")   
+# ...more html tags....
+    print("    <div>${get_user_input.description}</div>")
+    print("    <hr />")
+# ...more html tags...
+    print("</body>")
+    print("</html>")
+    
+def end_statements():
+    print("Web page was created successfully. ")
+    print("End of program. ")
+
+
+
 main()
 
 
-# Create HTML file and formatting using user input
-#def create_website(name, description):
-   # <!DOCTYPE html>
-   #<html>
-   # <head>
-   # </head>
-   # <body>
-    #    #<center>
-            #<h1>name</h1>
-        #</center>
-       # <hr />
-       # description 
-    
